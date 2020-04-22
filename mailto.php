b9253d61693e60b8a25ecb4ec7ceef83d0207eac
miyakooti <60727025+miyakooti@users.noreply.github.com>
2020-04-22 18:32:49 +0900

    Create mailto.php

---
+++ mailto.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>sample</title>
+</head>
+ 
+<body>
+ 
+<h2>メール送信完了</h2>    
+ 
+<p class="message">
+お問い合わせありがとうございます。1営業日以内にご返信させていただきます。<br>
+自動返信メールをお送りしておりますのでご確認ください。<br>
+1時間たっても届かない場合はお手数ですがこちらからご連絡ください。
+</p>
+</body>
+    
+</html>
